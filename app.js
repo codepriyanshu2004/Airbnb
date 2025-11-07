57884e72512e3bedf547b5cd2edd544b3e161622
codepriyanshu2004 <priyanshubitturajs@gmail.com>
2025-11-07 20:18:35 +0530

    first view ejs file

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const Listening = require("./models/listing.js")
+const path = require("path");
 
 const app= express()
 let port = 8080;
@@ -24,23 +25,31 @@ app.get("/",(req,res)=>{
     
 })
 
-app.get("/testListing", async(req,res)=>{
+// app.get("/testListing", async(req,res)=>{
 
-    let sampleListting = new Listening({
-        title: "My New Villa",
-        description:"By the beach",
-        price:1200,
-        location:"Calangute ,Goa",
-        country:"India"
-    });
+//     let sampleListting = new Listening({
+//         title: "My New Villa",
+//         description:"By the beach",    // for tesing only
+//         price:1200,
+//         location:"Calangute ,Goa",
+//         country:"India"
+//     });
 
-       await sampleListting.save();
-        console.log("sample was saved");
-        res.send("sucessfull test");
+//        await sampleListting.save();
+//         console.log("sample was saved");
+//         res.send("sucessfull test");
 
         
-   });
+//    });
 
+
+app.set("view engine","ejs");
+
+
+app.get("/listing",async(req,res)=>{
+   const allListings = await Listening.find({});
+   res.render("listings/index.ejs",{allListings});
+})
 
 
 app.listen(port,()=>{
