003bb593d9e7bbdd22c2f8cec4119a8ece81f111
codepriyanshu2004 <priyanshubitturajs@gmail.com>
2025-11-07 19:01:20 +0530

    create listing model and tested the create opeation

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const Listening = require("./models/listing.js")
 
 const app= express()
 let port = 8080;
@@ -22,6 +23,25 @@ app.get("/",(req,res)=>{
     console.log("You are in the root");
     
 })
+
+app.get("/testListing", async(req,res)=>{
+
+    let sampleListting = new Listening({
+        title: "My New Villa",
+        description:"By the beach",
+        price:1200,
+        location:"Calangute ,Goa",
+        country:"India"
+    });
+
+       await sampleListting.save();
+        console.log("sample was saved");
+        res.send("sucessfull test");
+
+        
+   });
+
+
 
 app.listen(port,()=>{
     console.log("Server is listening at 3000");
---
+++ models/listing.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const listingSchema = new mongoose.Schema({
+    title:{
+        type:String
+
+    },
+
+    description:{
+        type:String
+    },
+    image:{
+        type:String,
+        default : "https://unsplash.com/photos/a-branch-of-a-tree-with-white-flowers-in-front-of-a-body-of-water-FiWw3RgBRlY",
+        set:(v) => v=== "" ? "https://unsplash.com/photos/a-branch-of-a-tree-with-white-flowers-in-front-of-a-body-of-water-FiWw3RgBRlY": v,
+    },
+
+    price:{
+        type:Number,
+       
+    },
+
+    location:{
+        type:String
+    },
+
+    country:{
+        type:String
+    }
+
+
+});
+
+
+const Listing = mongoose.model("Listing",listingSchema);
+
+module.exports = Listing;
+
+
